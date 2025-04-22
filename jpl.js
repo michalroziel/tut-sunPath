267c4bc5e41ebcc241ab2b6e18d489ebb56fbce3
Michał Roziel <miro00009@htwsaar.de>
2025-04-22 12:58:49 +0200

    update

--- jpl.js
+++ jpl.js
@@ -1,7 +1,28 @@
 
 export function convertCoordination(){
 
-    console.log("convert coordination");
+    const toRad = (degree) => degree * Math.PI / 180;
+
+    const toDeg = (rad) => rad * 180 / Math.PI;
 
 }
 
+export function parseSunposition(azi, alt){
+
+    const azimuth = Number.parseFloat(azi);
+    const altitude = Number.parseFloat(alt);
+
+    if (Number.isNaN(azimuth) || Number.isNaN(altitude) ){
+
+        throw RangeError("Invalid input");
+    }
+
+
+    return {
+
+        azimuth, altitude
+
+    };
+
+
+}
