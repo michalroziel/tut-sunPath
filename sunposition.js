6e544fd398cc797f52efee03d5100bd8099f2b48
Michał Roziel <miro00009@htwsaar.de>
2025-04-22 12:43:12 +0200

    update

--- sunposition.js
+++ sunposition.js
@@ -3,12 +3,29 @@ import {convertCoordination} from "./jpl.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    const azimuthInput = document.getElementById("azimuth").value;
-    console.log(azimuthInput);
 
 
 
-    convertCoordination();
+    const okBtn = document.getElementById("ok");
+
+    okBtn.addEventListener("click", () => {
+
+        const azimuth = document.getElementById("azimuth").value;
+        const alititude = document.getElementById("altitude").value;
+        console.log({azimuth, alititude});
+
+        const v  = {azimuth, alititude};
+
+        const u = {
+            "azimuth" : azimuth,
+            "altitude": alititude,
+        }
+
+
+    });
+
+
+    //convertCoordination();
 
 });
 
